3b53a69f6965cd3cb32cb8e1f2756aaeccc03822
Sa7dera <san.kuz@o2.pl>
2024-11-10 16:44:05 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int liczba1 = 12;
+        int liczba2 = 5;
+
+        int suma = liczba1 + liczba2;
+        System.out.println("Suma liczb 12 i 5: " + suma);
+
+        int roznica = liczba1 - liczba2;
+        System.out.println("Roznica liczb 5 i 12: " + roznica);
+
+        int iloczyn = liczba1 * liczba2;
+        System.out.println("iloczyn liczb 12 i 5: " + iloczyn);
+
+        int iloraz = liczba1 / liczba2;
+        System.out.println("iloraz liczb 12 i 5: " + iloraz);
+
+        int resztaZdzielenia = liczba1 % liczba2;
+        System.out.println("reszta z dzielenia liczb 12 i 5: " + resztaZdzielenia);
+
+
+    }
+}
